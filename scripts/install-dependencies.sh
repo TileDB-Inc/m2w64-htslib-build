b623bb01c4eef32a87944c8e06d4e781c4173356
John Blischak <jdblischak@gmail.com>
2024-07-10 15:17:36 -0400

    Follow the exact same order as upstream
    https://github.com/LizardByte/Sunshine/blob/722e5600c6a15a605b5f07e17a17045fb19290cd/.github/workflows/CI.yml#L991-L995

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,7 +1,14 @@
 # https://github.com/samtools/htslib/blob/463830bf7de8c4ab731c4d67c49ddc446f498f50/INSTALL#L276
 # https://github.com/samtools/htslib/blob/develop/.appveyor.yml
 
+# Downgrade curl to version that can be statically linked
+# https://github.com/msys2/MINGW-packages/issues/21028
+# https://github.com/LizardByte/Sunshine/issues/2722#issuecomment-2177325777
+wget https://repo.msys2.org/mingw/ucrt64/mingw-w64-ucrt-x86_64-curl-8.8.0-1-any.pkg.tar.zst
+pacman -U --noconfirm mingw-w64-ucrt-x86_64-curl-8.8.0-1-any.pkg.tar.zst
+
 pacman -S --noconfirm --needed \
+  --ignore=mingw-w64-ucrt-x86_64-curl \
   base-devel \
   mingw-w64-x86_64-toolchain \
   mingw-w64-x86_64-autotools \
@@ -11,9 +18,3 @@ pacman -S --noconfirm --needed \
   mingw-w64-x86_64-curl \
   mingw-w64-x86_64-tools-git \
   mingw-w64-x86_64-libdeflate
-
-# Downgrade curl to version that can be statically linked
-# https://github.com/msys2/MINGW-packages/issues/21028
-# https://github.com/LizardByte/Sunshine/issues/2722#issuecomment-2177325777
-wget https://repo.msys2.org/mingw/ucrt64/mingw-w64-ucrt-x86_64-curl-8.8.0-1-any.pkg.tar.zst
-pacman -U --noconfirm mingw-w64-ucrt-x86_64-curl-8.8.0-1-any.pkg.tar.zst
