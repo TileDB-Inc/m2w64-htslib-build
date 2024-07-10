092f80615e31455544ecab58b65e6264ec71cdbb
John Blischak <jdblischak@gmail.com>
2024-07-10 14:44:13 -0400

    Install mingw-w64-ucrt-x86_64-curl-8.8.0-1-any

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -11,3 +11,9 @@ pacman -S --noconfirm --needed \
   mingw-w64-x86_64-curl \
   mingw-w64-x86_64-tools-git \
   mingw-w64-x86_64-libdeflate
+
+# Downgrade curl to version that can be statically linked
+# https://github.com/msys2/MINGW-packages/issues/21028
+# https://github.com/LizardByte/Sunshine/issues/2722#issuecomment-2177325777
+wget https://repo.msys2.org/mingw/ucrt64/mingw-w64-ucrt-x86_64-curl-8.8.0-1-any.pkg.tar.zst
+pacman -U --noconfirm mingw-w64-ucrt-x86_64-curl-8.8.0-1-any.pkg.tar.zst
