efceb308f843e9f5af45f802f7fbb9c64660d0a2
John Blischak <jdblischak@gmail.com>
2024-07-10 15:50:44 -0400

    --disable-libcurl

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,14 +1,7 @@
 # https://github.com/samtools/htslib/blob/463830bf7de8c4ab731c4d67c49ddc446f498f50/INSTALL#L276
 # https://github.com/samtools/htslib/blob/develop/.appveyor.yml
 
-# Downgrade curl to version that can be statically linked
-# https://github.com/msys2/MINGW-packages/issues/21028
-# https://github.com/LizardByte/Sunshine/issues/2722#issuecomment-2177325777
-wget https://repo.msys2.org/mingw/ucrt64/mingw-w64-ucrt-x86_64-curl-8.8.0-1-any.pkg.tar.zst
-pacman -U --noconfirm mingw-w64-ucrt-x86_64-curl-8.8.0-1-any.pkg.tar.zst
-
 pacman -S --noconfirm --needed \
-  --ignore=mingw-w64-ucrt-x86_64-curl \
   base-devel \
   mingw-w64-x86_64-toolchain \
   mingw-w64-x86_64-autotools \
@@ -20,4 +13,3 @@ pacman -S --noconfirm --needed \
   mingw-w64-x86_64-libdeflate
 
 pacman -Q
-pacman -Ss curl
