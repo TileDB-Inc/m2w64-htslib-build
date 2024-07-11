2c4ae954f5e8d4c5aef61a6531b251b6905a3379
John Blischak <jdblischak@gmail.com>
2024-07-11 16:20:39 -0400

    Update config.mk patch for --disable-libcurl

--- scripts/build-htslib.sh
+++ scripts/build-htslib.sh
@@ -4,7 +4,11 @@ PATCH_DIR="$1"
 
 LIBHTS_SOVERSION=${LIBHTS_SOVERSION-3}
 
+./configure
+mv config.mk config.mk.libcurl
 ./configure --disable-libcurl
+cat config.mk
+diff config.mk config.mk.libcurl
 
 # apply patches
 patch Makefile "${PATCH_DIR}/makefile.staticlink.patch"
